1cb5015b81b3c83da742328c8732a0cb17d5582b
Stefan Müller zzeroo <co@zzeroo.com>
2020-06-17 17:22:47 +0200

    Better package names
    The name of the package and the version are extracted from cargo.

--- package.sh
+++ package.sh
@@ -9,27 +9,27 @@ for ARCH in i686 x86_64; do
   source ~/.cargo/env
   cargo build --target=${ARCH}-pc-windows-gnu --release
   # extract package name and version from cargo
-  export NAME=$(cargo pkgid | cut -d# -f1 | cut -d\/ -f7)
-  export VERSION=$(cargo pkgid | cut -d# -f2)
-  export NAME_VERSION=${NAME}-${VERSION}
+  export NAME=$(cargo pkgid | cut -d# -f2 | cut -d: -f1)
+  export VERSION=$(cargo pkgid | cut -d# -f2 | cut -d: -f2)
+  export NAME_VERSION="${NAME}-${VERSION}"
   # create destination directory
-  mkdir ${NAME_VERSION}-windows-${ARCH}
-  cp target/${ARCH}-pc-windows-gnu/release/*.exe ${NAME_VERSION}-windows-${ARCH}
+  mkdir -p "${NAME_VERSION}-windows-${ARCH}"
+  cp target/${ARCH}-pc-windows-gnu/release/*.exe "${NAME_VERSION}-windows-${ARCH}"
   # extract all dependencies to libs
-  export DLLS=`peldd ${NAME_VERSION}-windows-${ARCH}/*.exe -t --ignore-errors`
+  export DLLS=`peldd "${NAME_VERSION}-windows-${ARCH}"/*.exe -t --ignore-errors`
   for DLL in $DLLS
-      do cp "$DLL" ${NAME_VERSION}-windows-${ARCH}
+      do cp "$DLL" "${NAME_VERSION}-windows-${ARCH}"
   done
   # copy the gtk and additional files like the LICENSE and the README
-  mkdir -p ${NAME_VERSION}-windows-${ARCH}/share/{themes,gtk-3.0}
-  cp -r $GTK_INSTALL_PATH/share/glib-2.0/schemas ${NAME_VERSION}-windows-${ARCH}/share/glib-2.0
-  cp -r $GTK_INSTALL_PATH/share/icons ${NAME_VERSION}-windows-${ARCH}/share/icons
-  [ -d resources ] && cp -r resources ${NAME_VERSION}-windows-${ARCH}/
-  [ -d share ] && cp -r share ${NAME_VERSION}-windows-${ARCH}/
-  [ -f README.md ] && cp -r README.md ${NAME_VERSION}-windows-${ARCH}/
-  [ -f LICENSE ] && cp -r LICENSE ${NAME_VERSION}-windows-${ARCH}/
+  mkdir -p "${NAME_VERSION}-windows-${ARCH}"/share/{themes,gtk-3.0}
+  cp -r $GTK_INSTALL_PATH/share/glib-2.0/schemas "${NAME_VERSION}-windows-${ARCH}"/share/glib-2.0
+  cp -r $GTK_INSTALL_PATH/share/icons "${NAME_VERSION}-windows-${ARCH}"/share/icons
+  [ -d resources ] && cp -r resources "${NAME_VERSION}-windows-${ARCH}"/
+  [ -d share ] && cp -r share "${NAME_VERSION}-windows-${ARCH}"/
+  [ -f README.md ] && cp -r README.md "${NAME_VERSION}-windows-${ARCH}"/
+  [ -f LICENSE ] && cp -r LICENSE "${NAME_VERSION}-windows-${ARCH}"/
   # reduce the binary size
-  mingw-strip ${NAME_VERSION}-windows-${ARCH}/*
+  mingw-strip "${NAME_VERSION}-windows-${ARCH}"/*
   # zip the whole package dir
-  zip -r ${NAME_VERSION}-windows-${ARCH}.zip ${NAME_VERSION}-windows-${ARCH}/*
+  zip -r "${NAME_VERSION}-windows-${ARCH}".zip "${NAME_VERSION}-windows-${ARCH}"/*
 done
